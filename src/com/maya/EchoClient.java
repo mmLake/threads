8c6178dec1ee3bc5484edacd2c86f6dad0d7793a
Maya Lake <abcdcookiemonster@gmail.com>
2017-09-28 19:00:48 -0700

    EchoServer and EchoClient working

--- src/com/maya/EchoClient.java
+++ src/com/maya/EchoClient.java
@@ -1,21 +1,59 @@
 package com.maya;
 
-import java.io.InputStream;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
+/**
+ * Client listens on a port hardcoded to 60010. Client inputs a String that will be
+ * sent to the server and echoed back. When the client enters the string "exit", the
+ * server closes the connection.
+ */
 
-public final class EchoClient {
+public class Main {
 
-    public static void main(String[] args) throws Exception {
-        try (Socket socket = new Socket("localhost", 22222)) {
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-            System.out.println(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = null;
+        PrintWriter out = null;
+        BufferedReader userInput = null;
+        Socket sock = null;
+
+        try {
+            sock = new Socket("localhost", 60010);
+
+            in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            userInput = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(sock.getOutputStream(),true);
+
+            while (true) {
+                System.out.print("Client> ");
+                String line = userInput.readLine();
+                if(line == null)
+                    break;
+                if (line.equals(""))
+                    break;
+                if (line.equalsIgnoreCase("Exit"))
+                    break;
+                out.println(line);
+                System.out.println(in.readLine());
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        } finally {
+            if (in != null)
+                in.close();
+            if (userInput != null)
+                userInput.close();
+            if (out != null)
+                out.close();
+            if (sock != null)
+                sock.close();
         }
     }
+
 }
+
 
 
 
--- src/com/maya/EchoServer.java
+++ src/com/maya/EchoServer.java
@@ -1,23 +1,101 @@
 package com.maya;
 
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 
-public final class EchoServer {
+/**
+ * Server listens on a port hardcoded to 60010. This server reads from the client
+ * and echoes back the result. When the client enters the string "exit", the
+ * server closes the connection.
+ */
 
-    public static void main(String[] args) throws Exception {
-        try (ServerSocket serverSocket = new ServerSocket(22222)) {
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Main main = new Main();
+        main.start();
+    }
+
+    private void start(){
+        ServerSocket socket = null;
+
+        try {
+            // set socket to port 60010
+            socket = new ServerSocket(60010);
+
+            /*
+             * When a client connects to server, a new thread is created that
+             * will interact with the client
+             */
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-                }
+                // listening for connection
+                Socket client = socket.accept();
+                String address = client.getInetAddress().getHostAddress();
+                System.out.printf("Client connected: %s%n", address);
+
+                // create a new thread then start it
+                Threads newThread = new Threads(client);
+                newThread.start();
+            }
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        } finally {
+            try {
+                if (socket != null)
+                    socket.close();
+            } catch (IOException ioee) {
+                System.err.println(ioee);
             }
         }
     }
+
+    public class Threads extends Thread {
+        public Threads(Socket c) {
+            client = c;
+        }
+
+        public void run() {
+            BufferedReader networkInput = null;
+            OutputStreamWriter networkOutput = null;
+
+            try {
+                networkInput = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                networkOutput = new OutputStreamWriter(client.getOutputStream());
+
+                //read from input and echo same words back to output
+                boolean exitThread = false;
+                while (!exitThread) {
+                    String line = networkInput.readLine();
+                    if ( (line == null) || line.equalsIgnoreCase("exit") ) {
+                        exitThread = true;
+                    }
+                    else
+                        networkOutput.write("Server> "+line+"\n");
+
+                    networkOutput.flush();
+                }
+            } catch (IOException ioe) {
+                System.err.println(ioe);
+            } finally {
+                try {
+                    if (networkInput != null)
+                        networkInput.close();
+                    if (networkOutput != null)
+                        networkOutput.close();
+                    if (client != null)
+                        client.close();
+
+                    String address = client.getInetAddress().getHostAddress();
+                    System.out.printf("Client disconnected: %s%n", address);
+                } catch (IOException ioee) {
+                    System.err.println(ioee);
+                }
+            }
+        }
+        private Socket client;
+    }
+
 }
+
+
+
